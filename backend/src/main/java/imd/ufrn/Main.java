21f5c72d4060d24d0f460b77fc401f58d35ba6ff
L-Marcel <lmgh1312@gmail.com>
2025-09-11 08:39:59 -0300

    [chore] Tranzendo aquivo de testes do jmeter

--- backend/src/main/java/imd/ufrn/Main.java
+++ backend/src/main/java/imd/ufrn/Main.java
@@ -7,21 +7,21 @@ import imd.ufrn.enums.TransportProtocol;
 
 public class Main {
   public static void main(String[] args) {
-    // DawnSettings settings = new DawnSettings(
-    //   TransportProtocol.TCP,
-    //   8080,
-    //   Duration.ofSeconds(1),
-    //   true,
-    //   5000
-    // );
-
     DawnSettings settings = new DawnSettings(
-      TransportProtocol.UDP,
+      TransportProtocol.TCP,
       8080,
       Duration.ofSeconds(1),
-      false,
-      0
+      true,
+      5000
     );
+
+    // DawnSettings settings = new DawnSettings(
+    //   TransportProtocol.UDP,
+    //   8080,
+    //   Duration.ofSeconds(1),
+    //   false,
+    //   0
+    // );
 
     Dawn dawn = new Dawn(settings);
     dawn.addController(ProductsController.class);
